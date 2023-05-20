1f0efdf8167c01ab86c263e18b39e7401f512dec
Merykson <merykson.acacio@hotmail.com>
2023-05-20 14:05:37 -0300

    feat: add github pages #master

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
+    base: "/mental-health-frontend/",
+    build: {
+        outDir: "dist",
+        assetsDir: "./",
+        sourcemap: true,
+        emptyOutDir: true,
+    },
     plugins: [vue()],
 });
